a62985d6fd309884aa0cb419d5f222480db21ccf
Roarke <willman0031@gmail.com>
2020-06-19 09:56:02 +0700

    First-commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolClientId: 'YOUR_CLIENT_ID', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'YOUR_REGION', // e.g. us-east-2
+        authDomainPrefix: 'YOUR_AUTH_DOMAIN' // wildrydes-sapessi
+    },
+    api: {
+        invokeUrl: 'YOUR_API_ENDPOINT' // e.g. https://xxxxxxxx.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
